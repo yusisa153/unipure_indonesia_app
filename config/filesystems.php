96ce5a90fba09744d4dc825e1b4e8d66e3a0e42e
yusisa153 <yusisa153@gmail.com>
2024-12-20 18:07:26 +0700

    perbaikan lagi

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app/private'),
+            'url' => env('APP_URL').'/storage',
             'serve' => true,
             'throw' => false,
         ],
